bef64e77bb43dcb28821ae10bdcc2aec5c5e3e4c
Wiltrouble <wiltrouble@gmail.com>
2021-03-21 18:30:16 -0400

    fix login

--- components/account/LoginForm.js
+++ components/account/LoginForm.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Button, Icon, Input } from "react-native-elements";
-import { useNavigation } from "@react-navigation/native";
-import { size } from "lodash";
+import { Button, Icon, Input } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+import { isEmpty } from 'lodash'
 
-import { validateEmail } from "../../utils/helpers";
-import { registerUser } from "../../utils/actions";
-import { Loading } from "../../components/Loading";
+import { validateEmail } from '../../utils/helpers'
+import { loginWithEmailAndPassword } from '../../utils/actions'
+import Loading from '../Loading'
 
 export default function LoginForm() {
     const [showPassword, setShowPassword] = useState(false)
@@ -15,77 +15,108 @@ export default function LoginForm() {
     const [errorPassword, setErrorPassword] = useState("")
     const [loading, setLoading] = useState(false)
 
-    const navigation = useNavigation();
+    const navigation = useNavigation()
 
     const onChange = (e, type) => {
         setFormData({ ...formData, [type]: e.nativeEvent.text })
     }
 
-    const login = () =>{
-        console.log("login");
+    const doLogin = async() => {
+        if (!validateData()) {
+            return;
+        }
+
+        setLoading(true)
+        const result = await loginWithEmailAndPassword(formData.email, formData.password)
+        setLoading(false)
+
+        if (!result.statusResponse) {
+            setErrorEmail(result.error)
+            setErrorPassword(result.error)
+            return
+        }
+
+        navigation.navigate("account")
     }
+
+    const validateData = () => {
+        setErrorEmail("")
+        setErrorPassword("")
+        let isValid = true
+
+        if(!validateEmail(formData.email)) {
+            setErrorEmail("Debes de ingresar un email válido.")
+            isValid = false
+        }
+
+        if (isEmpty(formData.password)) {
+            setErrorPassword("Debes de ingresar tu contraseña.")
+            isValid = false
+        }
+
+        return isValid
+    }
+
 
     return (
         <View style={styles.container}>
             <Input
-                placeholder="Email"
                 containerStyle={styles.input}
-                onChange={ (e) => onChange(e, "email")}
+                placeholder="Ingresa tu email..."
+                onChange={(e) => onChange(e, "email")}
                 keyboardType="email-address"
                 errorMessage={errorEmail}
                 defaultValue={formData.email}
             />
             <Input
-                placeholder = "Password"
-                containerStyle = {styles.input}
-                password = {true}
-                secureTextEntry  = {!showPassword}
+                containerStyle={styles.input}
+                placeholder="Ingresa tu contraseña..."
+                password={true}
+                secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "password")}
                 errorMessage={errorPassword}
                 defaultValue={formData.password}
-                onChange={ (e) => onChange(e, "password")}
-                rightIcon={<Icon
-                    type = "material-community"
-                    name = { showPassword? "eye-off-outline" : "eye-outline" }
-                    iconStyle={styles.icon}
-                    onPress = {() => setShowPassword(!showPassword)}
-                />}
-
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={ showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
             />
             <Button
-                title = "Log in"
-                containerStyle = {styles.btnContainer}
-                buttonStyle = {styles.button}
-                onPress = { () => login()}
+                title="Iniciar Sesión"
+                containerStyle={styles.btnContainer}
+                buttonStyle={styles.btn}
+                onPress={() => doLogin()}
             />
-            <Loading isVisible = {loading} text = "Loading..."/>
+            <Loading isVisible={loading} text="Iniciando Sesión..."/>
         </View>
     )
 }
 
 const defaultFormValues = () => {
-    return {
-        email: '',
-        password: ''
-    }
+    return { email: "", password: "" }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         marginTop: 30
     },
     input: {
-        width: '100%',
-    },
+        width: "100%"
+    },  
     btnContainer: {
         marginTop: 20,
         width: "95%",
-        alignSelf: 'center'
+        alignSelf: "center"
     },
-    button: {
-        backgroundColor: '#442484'
+    btn: {
+        backgroundColor: "#442484"
     },
     icon: {
         color: "#c1c1c1"
